dc27f37f40fe4607ac5468d3dc27ccf25f8d26a4
PeMwaura <peterbundiwakonyo@gmail.com>
2024-02-24 14:12:55 +0300

    python code

---
+++ Desktop/PLP_Python/user_input.py
@@ -0,0 +1,5 @@
+first_name = input('Enter your first name:');
+last_name = input('Enter your last name:');
+age = input ('Enter your age:');
+location = input('Enter your location:');
+print('Hello ' + first_name + ' ' + last_name + ', you are ' + age + ' years old, you live in ' + location);
