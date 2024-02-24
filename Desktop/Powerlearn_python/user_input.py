0b73988dd9fbdcb1178fd0d04e859dd1dacac3fd
PeMwaura <peterbundiwakonyo@gmail.com>
2024-02-24 14:01:03 +0300

    python_assignment

--- Desktop/Powerlearn_python/user_input.py
+++ Desktop/Powerlearn_python/user_input.py
@@ -1,5 +1,5 @@
-first_name = input('enter your first name:');
-last_name = input('enter your last name:');
-age = input ('enter your age:');
-location = input('enter your location:');
+first_name = input('Enter your first name:');
+last_name = input('Enter your last name:');
+age = input ('Enter your age:');
+location = input('Enter your location:');
 print('Hello ' + first_name + ' ' + last_name + ', you are ' + age + ' years old, you live in ' + location);
