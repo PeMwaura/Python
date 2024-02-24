837a68281cecc6981b719b05501aa6574660c1f9
PeMwaura <peterbundiwakonyo@gmail.com>
2024-02-24 13:46:34 +0300

    python week one assignment

---
+++ Desktop/Powerlearn_python/user_input.py
@@ -0,0 +1,5 @@
+first_name = input('enter your first name:');
+last_name = input('enter your last name:');
+age = input ('enter your age:');
+location = input('enter your location:');
+print('Hello ' + first_name + ' ' + last_name + ', you are ' + age + ' years old, you live in ' + location);
